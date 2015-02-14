831ffc7e4eadec83fef884620586fe3775a0ecd5
Riza Fahmi <rizafahmi@gmail.com>
2015-02-14 08:44:25 +0700

    Remove boilerplate code

--- keyboardhero.js
+++ keyboardhero.js
@@ -1,18 +1,16 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+    // counter: function () {
+    //   return Session.get('counter');
+    // }
   });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+    // 'click button': function () {
+    //   Session.set('counter', Session.get('counter') + 1);
+    // }
   });
 }
 
